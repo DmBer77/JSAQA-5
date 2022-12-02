f50ae17461bdb0b2189e02f88960f6ce40773a5a
Dmitry Berezovsky <dm.berezovsky@gmail.com>
2022-12-02 14:38:27 +0300

    task 1 - ok

--- ticket.test.js
+++ ticket.test.js
@@ -1,3 +1,4 @@
+const { clickElement } = require('./lib/commands.js');
 let page;
 
 beforeEach(async () => {
@@ -12,14 +13,16 @@ afterEach(() => {
 
 describe('Movie tickets tests', () => {
     test('First test - positive', async () => {
-        await page.click('body > nav > a:nth-child(4)');
-        await page.click(
+        await clickElement(page, 'body > nav > a:nth-child(4)');
+        await clickElement(
+            page,
             'body > main > section:nth-child(2) > div:nth-child(2) > ul > li > a',
         );
-        await page.click(
+        await clickElement(
+            page,
             'body > main > section > div.buying-scheme > div.buying-scheme__wrapper > div:nth-child(7) > span:nth-child(5)',
         );
-        await page.click('body > main > section > button');
+        await clickElement(page, 'body > main > section > button');
 
         const btnSelector = '.acceptin-button';
         await page.waitForSelector(btnSelector, {
@@ -30,14 +33,16 @@ describe('Movie tickets tests', () => {
     });
 
     test('Second test - positive', async () => {
-        await page.click('body > nav > a:nth-child(7)');
-        await page.click(
+        await clickElement(page, 'body > nav > a:nth-child(7)');
+        await clickElement(
+            page,
             'body > main > section:nth-child(3) > div:nth-child(2) > ul > li > a',
         );
-        await page.click(
+        await clickElement(
+            page,
             'body > main > section > div.buying-scheme > div.buying-scheme__wrapper > div:nth-child(8) > span:nth-child(5)',
         );
-        await page.click('body > main > section > button');
+        await clickElement(page, 'body > main > section > button');
 
         const btnSelector = '.acceptin-button';
         await page.waitForSelector(btnSelector, {
@@ -48,10 +53,12 @@ describe('Movie tickets tests', () => {
     });
 
     test('Third text - negative', async () => {
-        await page.click(
+        await clickElement(
+            page,
             'body > nav > a.page-nav__day.page-nav__day_today.page-nav__day_chosen',
         );
-        await page.click(
+        await clickElement(
+            page,
             'body > main > section:nth-child(3) > div:nth-child(3) > ul > li > a',
         );
 
