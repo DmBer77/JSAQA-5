523c207b4a706c4e66a4bdbdf0edd44ebbc20036
Dmitry Berezovsky <dm.berezovsky@gmail.com>
2022-12-13 17:08:27 +0300

    task 1 - ok (fixed)

--- ticket.test.js
+++ ticket.test.js
@@ -1,5 +1,12 @@
-const { clickElement } = require('./lib/commands.js');
+const {
+    clickElement,
+    clickSeat,
+    spotTheDay,
+    spotTheTime,
+} = require('./lib/commands.js');
 let page;
+const btnBook = 'body > main > section > button';
+const btnGetTheCode = '.acceptin-button';
 
 beforeEach(async () => {
     page = await browser.newPage();
@@ -13,60 +20,52 @@ afterEach(() => {
 
 describe('Movie tickets tests', () => {
     test('First test - positive', async () => {
-        await clickElement(page, 'body > nav > a:nth-child(4)');
-        await clickElement(
-            page,
-            'body > main > section:nth-child(2) > div:nth-child(2) > ul > li > a',
-        );
-        await clickElement(
-            page,
-            'body > main > section > div.buying-scheme > div.buying-scheme__wrapper > div:nth-child(7) > span:nth-child(5)',
-        );
-        await clickElement(page, 'body > main > section > button');
+        const day = await spotTheDay(4);
+        const time = await spotTheTime(2, 2);
 
-        const btnSelector = '.acceptin-button';
-        await page.waitForSelector(btnSelector, {
+        await clickElement(page, day);
+        await clickElement(page, time);
+        await clickSeat(page, 7, 5);
+        await clickElement(page, btnBook);
+
+        await page.waitForSelector(btnGetTheCode, {
             visible: true,
         });
-        const actual = await page.$eval(btnSelector, link => link.textContent);
+        const actual = await page.$eval(
+            btnGetTheCode,
+            link => link.textContent,
+        );
         expect(actual).toContain('Получить код бронирования');
     });
 
     test('Second test - positive', async () => {
-        await clickElement(page, 'body > nav > a:nth-child(7)');
-        await clickElement(
-            page,
-            'body > main > section:nth-child(3) > div:nth-child(2) > ul > li > a',
-        );
-        await clickElement(
-            page,
-            'body > main > section > div.buying-scheme > div.buying-scheme__wrapper > div:nth-child(8) > span:nth-child(5)',
-        );
-        await clickElement(page, 'body > main > section > button');
+        const day = await spotTheDay(7);
+        const time = await spotTheTime(1, 2);
 
-        const btnSelector = '.acceptin-button';
-        await page.waitForSelector(btnSelector, {
+        await clickElement(page, day);
+        await clickElement(page, time);
+        await clickSeat(page, 6, 5);
+        await clickElement(page, btnBook);
+
+        await page.waitForSelector(btnGetTheCode, {
             visible: true,
         });
-        const actual = await page.$eval(btnSelector, link => link.textContent);
+        const actual = await page.$eval(
+            btnGetTheCode,
+            link => link.textContent,
+        );
         expect(actual).toContain('Получить код бронирования');
     });
 
     test('Third text - negative', async () => {
-        await clickElement(
-            page,
-            'body > nav > a.page-nav__day.page-nav__day_today.page-nav__day_chosen',
+        const time = await spotTheTime(2, 3);
+
+        await page.waitForSelector(time, {
+            visible: true,
+        });
+        const actual = await page.$eval(time, link => link.className);
+        expect(actual).toContain(
+            'movie-seances__time acceptin-button-disabled',
         );
-        await clickElement(
-            page,
-            'body > main > section:nth-child(3) > div:nth-child(3) > ul > li > a',
-        );
-
-        const actualUrl = await page.url();
-        const actual = typeof actualUrl;
-
-        const targetUrl = 'http://qamid.tmweb.ru/client/hall.php';
-
-        expect(actual).not.toEqual(targetUrl);
     });
 });
