7126f78efdc829db5c1cf1dabf4f7963e91d70b7
Dmitry Berezovsky <dm.berezovsky@gmail.com>
2022-12-14 11:06:52 +0300

    task 2 - ok (fixed)

--- features/step_definitions/action.feature
+++ features/step_definitions/action.feature
@@ -1,5 +1,10 @@
 Feature: Tickets
 Scenario: Should bay ticket
     Given user is on "Идем в кино" page
-    When user click to "дата, время, место в зале, подтвердить"
+    When user clicks day 4
+        And user selects show 2 and time 2
+        And user clicks row 7 and seat 5
+        And user clicks submit button
     Then user sees the button with name "Получить код бронирования"
+
+
--- features/step_definitions/action.step.js
+++ features/step_definitions/action.step.js
@@ -3,7 +3,7 @@ const puppeteer = require('puppeteer');
 const expect = require('chai');
 const { assert } = require('chai');
 
-Before(async function () {
+Before({ timeout: 60 * 1000 }, async function () {
     const browser = await puppeteer.launch({ headless: false, slowMo: 80 });
     const page = await browser.newPage();
     this.browser = browser;
@@ -15,33 +15,54 @@ After(async function () {
     }
 });
 
-Given('user is on {string} page', async function (string) {
-    return await this.page.goto('http://qamid.tmweb.ru/client/index.php', {
-        setTimeout: 15000,
-    });
+Given(
+    'user is on {string} page',
+    { timeout: 60 * 1000 },
+    async function (string) {
+        return await this.page.goto('http://qamid.tmweb.ru/client/index.php', {
+            setTimeout: 60000,
+        });
+    },
+);
+
+When('user clicks day {int}', async function (int) {
+    const daySelector = 'body > nav > a:nth-child(' + int + ')';
+    await this.page.click(daySelector);
 });
 
-When('user click to {string}', async function (string) {
-    await this.page.click('body > nav > a:nth-child(4)');
-    await this.page.click(
-        'body > main > section:nth-child(2) > div:nth-child(2) > ul > li > a',
-    );
+When('user selects show {int} and time {int}', async function (int, int2) {
+    const timeSelector =
+        'body > main > section:nth-child(' +
+        int +
+        ') > div:nth-child(' +
+        int2 +
+        ') > ul > li > a';
+    await this.page.waitForSelector(timeSelector);
+    await this.page.click(timeSelector);
+});
 
-    await this.page.waitForSelector(
-        'body > main > section > div.buying-scheme > div.buying-scheme__wrapper > div:nth-child(7) > span:nth-child(5)',
-    );
-    await this.page.click(
-        'body > main > section > div.buying-scheme > div.buying-scheme__wrapper > div:nth-child(7) > span:nth-child(5)',
-    );
-    await this.page.click('body > main > section > button');
+When('user clicks row {int} and seat {int}', async function (int, int2) {
+    const seatSelector =
+        'body > main > section > div.buying-scheme > div.buying-scheme__wrapper > div:nth-child(' +
+        int +
+        ') > span:nth-child(' +
+        int2 +
+        ')';
+    await this.page.waitForSelector(seatSelector);
+    await this.page.click(seatSelector);
+});
+
+When('user clicks submit button', async function () {
+    const btnBook = 'body > main > section > button';
+    await this.page.waitForSelector(btnBook);
+    await this.page.click(btnBook);
 });
 
 Then('user sees the button with name {string}', async function (string) {
-    const btnSelector = '.acceptin-button';
+    const btnSelector = 'body > main > section > div > button';
     await this.page.waitForSelector(btnSelector, {
         visible: true,
     });
     const actual = await this.page.$eval(btnSelector, link => link.textContent);
-    // expect(actual).contain('Получить код бронирования');
     assert.equal(actual, 'Получить код бронирования');
 });
